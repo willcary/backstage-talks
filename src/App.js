4eef8ad85897c6d955b80d53a6153752300f4169
willcary <w.a.c.ary@gmail.com>
2022-03-09 11:27:45 -0500

    Remove Unused Variable

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react"
+import React, { useState } from "react"
 import Header from "./Components/Header"
 import Main from "./Components/Main"
 import Footer from "./Components/Footer"
